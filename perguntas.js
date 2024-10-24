1ff67e606ca01141e1b709da682d5aa74036a7b7
Franciele17 <145395450+franciele2007@users.noreply.github.com>
2024-10-24 14:53:59 -0300

    24/10

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Ciências',
+    '1)A terra tem três camadas que são diferentes devido às variações de temperatura. Quais são suas três camadas?',
+    'Crosta, Manto e Núcleo'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Ciências',
+    ' 2)Este homem é responsável por remodelar a forma como o homem primitivo acreditava que o sistema solar funcionava. Ele propôs que a Terra não era o centro do universo e que o Sol estava no centro do nosso sistema solar. Quem era ele?',
+    'Nicolaus Copernicus'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Ciências',
+    '3)Este planeta gira mais rápido, completando uma rotação inteira em apenas 10 horas. Qual planeta é? ',
+    'Júpiter'
 )
 
 criaCartao(
-    'Categoria',
-    'Pergunta',
-    'Resposta'
+    'Ciências',
+    '4)Os viajantes espaciais dos Estados Unidos são chamados de astronautas. Da Rússia, eles são chamados de cosmonautas. De onde são os taikonautas? ',
+    'China'
 )
